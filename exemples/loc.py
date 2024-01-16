5af0d8f73fe5e7660559431abd1ec4af878306fb
juliebriault <jubriault@gmail.com>
2024-01-16 12:20:00 +0100

    Creation de loc.py
    Script python pour localiser (et afficher la date, latitude, longitude)

---
+++ exemples/loc.py
@@ -0,0 +1,58 @@
+import re
+from time import sleep
+import serial
+
+def parse_gps_data(gps_data):
+    match = re.search(r'\+CGNSINF: \d,\d,(\d{14}\.\d+),([\d.]+),([\d.]+)', gps_data)
+   
+    if match:
+        timestamp = match.group(1)
+        latitude = match.group(2)
+        longitude = match.group(3)
+       
+        return timestamp, latitude, longitude
+    else:
+        return None
+
+# Connexion au port série (à adapter en fonction de votre configuration)
+#ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
+
+# Configuration du port série pour la communication avec le module BG96
+port = '/dev/ttyUSB0'  # Remplacez par le bon port série si nécessaire
+baudrate = 115200
+   
+ser = serial.Serial(port, baudrate, timeout=1)
+
+# Verification Connection
+ser.write(b'AT\r\n')
+sleep(1)
+lin = ser.readline().strip()
+line = ser.readline().strip()
+print(line)
+
+# Power On
+ser.write(b'AT+CGNSPWR=1 \r\n')
+sleep(5)
+lin = ser.readline().strip()
+line = ser.readline().strip()
+print(line)
+
+# Information Date, Latitude, Longitude
+ser.write(b'AT+CGNSINF\r\n')
+sleep(5)
+lin = ser.readline().strip()
+line = ser.readline().strip()
+print(line)
+
+
+# Fermeture du port série
+ser.close()
+
+# Affichage des données séparées
+parsed_data = parse_gps_data(line)
+
+if parsed_data:
+    timestamp, latitude, longitude = parsed_data
+    print(f"Date: {timestamp}, Latitude: {latitude}, Longitude: {longitude}")
+else:
+    print("Les données GPS n'ont pas pu être extraites.")
